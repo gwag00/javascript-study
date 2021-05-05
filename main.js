cd1d2bee6065c9154c6d3b8926ce8f269ab73116
gwag00 <kwag00@naver.com>
2021-05-05 23:09:13 +0900

    데이터 타입 확인

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log('Hello World!');
+import getType from './getType'
+
+console.log(typeof 'Hello World!')
+console.log(typeof 123)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof {})
+console.log(typeof [])
+
+console.log(getType(123))
+console.log(getType(false))
+console.log(getType(null))
