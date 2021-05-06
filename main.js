34c0829bdea80da9339ae19bc4ca95bb5534c2a3
gwag00 <kwag00@naver.com>
2021-05-06 15:31:04 +0900

    화살표함수, 즉시실행함수, 호이스팅, 타이머 함수, 콜백

--- main.js
+++ main.js
@@ -1,15 +1,71 @@
-// 반복문 (For statement)
-// for (시작조건; 종료조건; 변화조건) {}
+// 화살표 함수
+// () => {} vs function () {}
 
-const ulEl = document.querySelector('ul')
-
-for (let i = 0; i < 10; i+=1) {
-  const li = document.createElement('li')
-  li.textContent = `list-${i + 1}`
-  if ( (i + 1) % 2 === 0) {
-    li.addEventListener('click', function () {
-      console.log(li.textContent)
-    })
-  }
-  ulEl.appendChild(li)
+const double = function (x, y) {
+  return x * 2
 }
+console.log('double: ', double(7))
+
+const doubleArrow = x => x * 2
+console.log('doubleArrow', doubleArrow(7))
+
+// 즉시실행함수
+// IIFE, Immediately-Invoked Function Expression
+
+// 호이스팅(Hoisting)
+// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
+
+const a = 7
+
+double1();
+
+function double1() {
+  console.log(a * 2)
+}
+
+(function double1() {
+  console.log(a * 2)
+})();
+
+(function double1() {
+  console.log(a * 2)
+}());
+
+// 타이머 함수
+// setTimeout(함수, 시간): 일정 시간 후 함수 실행
+// setInterval(함수, 시간): 시간 간격마다 함수 실행
+// clearTimeout(): 설정된 Timeout 함수를 종료
+// clearInterval(): 설정된 Interval 함수를 종료
+
+const timer = setTimeout(() => {
+  console.log('HEROPY')
+}, 3000)
+
+const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  clearTimeout(timer)
+})
+
+const timer1 = setInterval(() => {
+  console.log('HEROPY1')
+}, 3000)
+
+const h1El1 = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+  clearInterval(timer1)
+})
+
+// 콜백(Callback)
+// 함수의 진수로 사용되는 함수
+
+// ex) setTimeout(함수, 시간)
+
+function timeout(cb) {
+  setTimeout(() => {
+    console.log('Heropy!')
+    cb()
+  }, 3000)
+}
+timeout(() => {
+  console.log('Done!')
+})
